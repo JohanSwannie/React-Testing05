0e090082c4f031b751a4582dd3248ef67a29886c
Johan Swan <jswannieman@gmail.com>
2023-12-11 08:39:55 +1300

    Update App.test JSX Component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -17,11 +17,7 @@ test("renders the text 'We are in Testing Mode'", () => {
 
 test("renders the text 'Fasten you Seatbelts!!!'", () => {
   render(<App />);
-  const textDisplay1 = document.getElementById("welcome").innerHTML;
-  const textDisplay2 = document.getElementById("mode").innerHTML;
   const textDisplay3 = document.getElementById("safety").innerHTML;
-  expect(textDisplay1).toMatch(/Welcome Home Folks/i);
-  expect(textDisplay2).toMatch(/We are in Testing Mode/i);
   expect(textDisplay3).toMatch(/Fasten you Seatbelts!!!/i);
-  expect(textDisplay1.length).toBeGreaterThanOrEqual(10);
+  expect(textDisplay3.length).toBeGreaterThanOrEqual(20);
 });
