5772747ec90be943131d037396a4f16503a2082d
Johan Swan <jswannieman@gmail.com>
2023-12-11 08:37:50 +1300

    Update App.test JSX Component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,11 +4,7 @@ import App from "./App";
 test("renders the text 'Welcome Home Folks'", () => {
   render(<App />);
   const textDisplay1 = document.getElementById("welcome").innerHTML;
-  const textDisplay2 = document.getElementById("mode").innerHTML;
-  const textDisplay3 = document.getElementById("safety").innerHTML;
   expect(textDisplay1).toMatch(/Welcome Home Folks/i);
-  expect(textDisplay2).toMatch(/We are in Testing Mode/i);
-  expect(textDisplay3).toMatch(/Fasten you Seatbelts!!!/i);
   expect(textDisplay1.length).toBeGreaterThanOrEqual(10);
 });
 
