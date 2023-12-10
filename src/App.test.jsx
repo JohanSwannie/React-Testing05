e4d46973d885d637c4252cd385507168e18b7d13
Johan Swan <jswannieman@gmail.com>
2023-12-11 08:36:58 +1300

    Update App.test JSX Component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -11,3 +11,25 @@ test("renders the text 'Welcome Home Folks'", () => {
   expect(textDisplay3).toMatch(/Fasten you Seatbelts!!!/i);
   expect(textDisplay1.length).toBeGreaterThanOrEqual(10);
 });
+
+test("renders the text 'We are in Testing Mode'", () => {
+  render(<App />);
+  const textDisplay1 = document.getElementById("welcome").innerHTML;
+  const textDisplay2 = document.getElementById("mode").innerHTML;
+  const textDisplay3 = document.getElementById("safety").innerHTML;
+  expect(textDisplay1).toMatch(/Welcome Home Folks/i);
+  expect(textDisplay2).toMatch(/We are in Testing Mode/i);
+  expect(textDisplay3).toMatch(/Fasten you Seatbelts!!!/i);
+  expect(textDisplay1.length).toBeGreaterThanOrEqual(10);
+});
+
+test("renders the text 'Fasten you Seatbelts!!!'", () => {
+  render(<App />);
+  const textDisplay1 = document.getElementById("welcome").innerHTML;
+  const textDisplay2 = document.getElementById("mode").innerHTML;
+  const textDisplay3 = document.getElementById("safety").innerHTML;
+  expect(textDisplay1).toMatch(/Welcome Home Folks/i);
+  expect(textDisplay2).toMatch(/We are in Testing Mode/i);
+  expect(textDisplay3).toMatch(/Fasten you Seatbelts!!!/i);
+  expect(textDisplay1.length).toBeGreaterThanOrEqual(10);
+});
