9772066c14574506abe1fd05cb2f78c7a4a674f8
Johan Swan <jswannieman@gmail.com>
2023-12-14 07:53:27 +1300

    Update main JSX Component

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootMan = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
+rootMan.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
